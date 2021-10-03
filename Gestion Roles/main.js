2ebfaf2298e89a4e94c87d9183d00ec70ebb0240
Diegob04 <diego.9350@hotmail.com>
2021-10-03 17:19:42 -0500

    Nueva version

--- Gestion Roles/main.js
+++ Gestion Roles/main.js
@@ -1,5 +1,5 @@
 
-class DataTable {
+class DataTable{
 
     element;
     headers;
@@ -10,13 +10,12 @@ class DataTable {
     numberOfEntries;
     headerButtons;
 
-    constructor(selector, headerButtons) {
+    constructor(selector, headerButtons){
         this.element = document.querySelector(selector);
 
         this.headers = [];
         this.items = [];
-        this.pagination = {
-            total: 0,
+        this.pagination = {total: 0,
             noItemsPerPage: 0,
             noPages: 0,
             actual: 0,
@@ -32,15 +31,15 @@ class DataTable {
     }
 
 
-    parse() {
+    parse(){
         const headers = [...this.element.querySelector("thead tr").children];
         const trs = [...this.element.querySelector("tbody").children];
 
-        headers.forEach(element => {
+        headers.forEach(element =>{
             this.headers.push(element.textContent);
         });
 
-        trs.forEach(tr => {
+        trs.forEach(tr =>{
             const cells = [...tr.children];
 
             const item = {
@@ -48,15 +47,15 @@ class DataTable {
                 values: []
             };
 
-            cells.forEach(cell => {
-                if (cell.children.length > 0) {
+            cells.forEach(cell =>{
+                if(cell.children.length > 0){
                     //const status = [...cell.children][0].getAtrribute("class");
                     const statusElement = [...cell.children][0];
                     const status = statusElement.getAttribute("class");
-                    if (status != null) {
+                    if(status != null){
                         item.values.push(`<span class="${status}"></span>`);
                     }
-                } else {
+                }else{
                     item.values.push(cell.textContent);
                 }
             });
@@ -69,7 +68,7 @@ class DataTable {
         this.makeTable();
     }
 
-    makeTable() {
+    makeTable(){
         this.copyItems = [...this.items];
 
         this.initPagination(this.items.length, this.numberOfEntries);
@@ -89,7 +88,7 @@ class DataTable {
         this.renderSelectEntries();
     }
 
-    initPagination(total, entries) {
+    initPagination(total, entries){
         this.pagination.total = total;
         this.pagination.noItemsPerPage = entries;
         this.pagination.noPages = Math.ceil(this.pagination.total / this.pagination.noItemsPerPage);
@@ -98,11 +97,11 @@ class DataTable {
         this.pagination.diff = this.pagination.noItemsPerPage - (this.pagination.total % this.pagination.noItemsPerPage);
     }
 
-    generateUUID() {
+    generateUUID(){
         return (Date.now() * Math.floor(Math.random() * 100000)).toString();
     }
 
-    createHTML() {
+    createHTML(){
         this.element.innerHTML = `
         <div class="datatable-container">
             <div class="header-tools">
@@ -136,48 +135,48 @@ class DataTable {
         </div>
         `;
     }
-    renderHeaders() {
+    renderHeaders(){
         this.element.querySelector("thead tr").innerHTML = "";
 
-        this.headers.forEach(header => {
-            this.element.querySelector("thead tr").innerHTML += `<th>${header}</th>`;
+        this.headers.forEach(header =>{
+           this.element.querySelector("thead tr").innerHTML += `<th>${header}</th>`;
         });
     }
-    renderRows() {
+    renderRows(){
         this.element.querySelector("tbody").innerHTML = "";
 
         let i = 0;
-        const { pointer, total } = this.pagination;
+        const {pointer, total} = this.pagination;
         const limit = this.pagination.actual * this.pagination.noItemsPerPage;
 
-        for (i = pointer; i < limit; i++) {
-            if (i == total) break;
+        for(i = pointer; i < limit; i++){
+            if(i == total) break;
 
-            const { id, values } = this.copyItems[i];
+            const{id, values} = this.copyItems[i];
             const checked = this.isChecked(id);
 
             let data = "";
 
             data += `<td class="table-checkbox">
-                        <input type="checkbox" class="datatable-checkbox" data-id="${id}" ${checked ? "checked" : ""} />
+                        <input type="checkbox" class="datatable-checkbox" data-id="${id}" ${checked? "checked":""} />
                     </td>`;
 
-            values.forEach(cell => {
+            values.forEach(cell =>{
                 data += `<td>${cell}</td>`
             });
 
             this.element.querySelector("tbody").innerHTML += `<tr>${data}</tr>`;
 
             //listener para el checkbox
-            document.querySelectorAll(".datatable-checkbox").forEach(checkbox => {
-                checkbox.addEventListener("click", e => {
+            document.querySelectorAll(".datatable-checkbox").forEach(checkbox =>{
+                checkbox.addEventListener("click", e =>{
                     const element = e.target;
                     const id = element.getAttribute("data-id");
 
-                    if (element.checked) {
+                    if(element.checked){
                         const item = this.getItem(id);
                         this.selected.push(item);
-                    } else {
+                    }else{
                         this.removeSelected(id);
                     }
 
@@ -188,30 +187,30 @@ class DataTable {
         }
     }
 
-    isChecked(id) {
+    isChecked(id){
         const items = this.selected;
         let res = false;
 
-        if (items.length == 0) return false;
+        if(items.length == 0) return false;
 
-        items.forEach(item => {
-            if (item.id == id) res = true;
+        items.forEach(item =>{
+            if(item.id == id) res = true;
         });
 
         return res;
     }
 
-    getItem(id) {
+    getItem(id){
         const res = this.items.filter(item => item.id == id);
-        if (res.length == 0) return null;
+        if(res.length == 0) return null;
         return res[0];
     }
 
-    removeSelected(id) {
+    removeSelected(id){
         const res = this.selected.filter(item => item.id != id);
-        this.selected = [...res];
+        this.selected = [... res];
     }
-    renderPagesButtons() {
+    renderPagesButtons(){
         const pagesContainer = this.element.querySelector(".pages");
         let pages = " ";
 
@@ -222,24 +221,24 @@ class DataTable {
         let limS = Math.min(actualIndex + 2, this.pagination.noPages);
         const missinButtons = buttonsToShow - (limS - limI);
 
-        if (Math.max(limI - missinButtons, 0)) {
+        if(Math.max(limI - missinButtons, 0)){
             limI = limI - missinButtons;
-        } else if (Math.min(limS + missinButtons, this.pagination.noPages) != this.pagination.noPages) {
+        }else if(Math.min(limS + missinButtons, this.pagination.noPages) != this.pagination.noPages){
             limS = limS + missinButtons;
         }
 
-        if (limS < (this.pagination.noPages - 2)) {
+        if(limS < (this.pagination.noPages - 2)){
             pages += this.getIteratedButtons(limI, limS);
             pages += "<li>...</li>";
             pages += this.getIteratedButtons(this.pagination.noPages - 1, this.pagination.noPages);
-        } else {
+        }else{
             pages += this.getIteratedButtons(limI, this.pagination.noPages);
         }
 
         pagesContainer.innerHTML = `<ul>${pages}</ul>`;
 
-        this.element.querySelectorAll(".pages li button").forEach(button => {
-            button.addEventListener("click", e => {
+        this.element.querySelectorAll(".pages li button").forEach(button =>{
+            button.addEventListener("click", e =>{
                 this.pagination.actual = parseInt(e.target.getAttribute("data-page"));
                 this.pagination.pointer = (this.pagination.actual * this.pagination.noItemsPerPage) - this.pagination.noItemsPerPage;
                 this.renderRows();
@@ -248,39 +247,39 @@ class DataTable {
         });
     }
 
-    getIteratedButtons(start, end) {
+    getIteratedButtons(start, end){
         let res = "";
-        for (let i = start; i <= end; i++) {
-            if (i == this.pagination.actual) {
+        for(let i = start; i <= end; i++){
+            if(i == this.pagination.actual){
                 res += `<li <span class="active">${i}</span></li>`;
-            } else {
+            }else{
                 res += `<li><button data-page="${i}">${i}</button></li>`;
             }
         }
 
         return res;
     }
-    renderHeaderButtons() {
+    renderHeaderButtons(){
         let html = " ";
         const buttonsContainer = this.element.querySelector(".header-buttons-container");
         const headerButtons = this.headerButtons;
 
-        headerButtons.forEach(button => {
+        headerButtons.forEach(button =>{
             html += `<li><button id="${button.id}"><i class=""material-icons">${button.icon}</i></button></li>`;
         });
 
         buttonsContainer.innerHTML = html;
 
-        headerButtons.forEach(button => {
+        headerButtons.forEach(button =>{
             document.querySelector("#" + button.id).addEventListener("click", button.action)
-        });
+        }); 
 
     }
-    renderSearch() {
-        this.element.querySelector(".search-input").addEventListener("input", e => {
+    renderSearch(){
+        this.element.querySelector(".search-input").addEventListener("input", e =>{
             const query = e.target.value.trim().toLowerCase();
 
-            if (query == "") {
+            if(query == ""){
                 this.copyItems = [... this.items];
                 this.initPagination(this.copyItems.length, this.numberOfEntries);
                 this.renderRows();
@@ -296,26 +295,26 @@ class DataTable {
         });
     }
 
-    search(query) {
+    search(query){
         let res = [];
 
         this.copyItems = [... this.items];
 
-        for (let i = 0; i < this.copyItems.length; i++) {
-            const { id, values } = this.copyItems[i];
-            const row = values;
+        for(let i = 0; i < this.copyItems.length; i++){
+        const {id, values} = this.copyItems[i];
+        const row = values;
 
-            for (let j = 0; j < row.length; j++) {
-                const cell = row[j];
+        for(let j = 0; j < row.length; j++){
+            const cell = row[j];
 
-                if (cell.toLowerCase().indexOf(query) >= 0) {
-                    res.push(this.copyItems[i]);
-                    break;
-                }
+            if(cell.toLowerCase().indexOf(query) >= 0){
+                res.push(this.copyItems[i]);
+                break;
             }
         }
+    }
 
-        this.copyItems = [...res];
+        this.copyItems = [... res];
     }
     renderSelectEntries() {
         const select = this.element.querySelector("#n-entries");
@@ -330,26 +329,26 @@ class DataTable {
             const numberOfEntries = parseInt(e.target.value);
             this.numberOfEntries = numberOfEntries;
 
-            this.initPagination(this.copyItems.length, this.numberOfEntries);
+            this.initPagination(this.copyItems.length,this.numberOfEntries);
             this.renderRows();
             this.renderPagesButtons();
             this.renderSearch();
         });
     }
 
-    getSelected() {
+    getSelected(){
         return this.selected;
     }
 
-    add(item) {
+    add(item){
         const row = {
             id: this.generateUUID(),
-            values: []
+            values:[]
         };
 
         const status = `<span class="${item[0]}"></span>`;
         item.shift();
-        row.values = [status, ...item];
+        row.values = [status, ... item];
         this.items = [row, ... this.items];
         this.makeTable();
     }
